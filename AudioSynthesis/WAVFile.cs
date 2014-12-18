cf65e9cbe843d6f4a4e3d3df2384342371e4d585
Nathaniel Graff <nathaniel.graff@gmail.com>
2014-12-18 15:18:38 -0800

    Overloaded generateTone function

--- AudioSynthesis/WAVFile.cs
+++ AudioSynthesis/WAVFile.cs
@@ -34,6 +34,16 @@ namespace AudioSynthesis
             }
         }
 
+        public static void addTones(int sampleCount, double[] tone_1, double[] tone_2, out double[] sum)
+        {
+            sum = new double[sampleCount];
+
+            for (var i = 0; i < sampleCount; i++)
+            {
+                sum[i] = tone_1[i] + tone_2[i];
+            }
+        }
+
         public static void generateTone(int sampleRate, Sinusoid func, double time, out double[] samples)
         {
             int sampleCount = (int)Math.Floor((double)sampleRate * time);
@@ -48,14 +58,24 @@ namespace AudioSynthesis
         public static void Main()
         {
             int sampleRate = 8000;
+            int sampleCount = 8000 * 2;
+
+            double[] tone_1, tone_2;
+            var func_1 = new Sinusoid();
+            func_1.Frequency = 440;
+            func_1.Amplitude = 0.5;
+            func_1.Phase = 90;
+
+            var func_2 = new Sinusoid();
+            func_2.Frequency = 698.46;
+            func_2.Amplitude = 0.45;
+            func_2.Phase = 0;
+
+            generateTone(sampleRate, func_1, 2.0, out tone_1);
+            generateTone(sampleRate, func_2, 2.0, out tone_2);
 
             double[] data;
-            var func = new Sinusoid();
-            func.Frequency = 440;
-            func.Amplitude = 0.5;
-            func.Phase = 90;
-
-            generateTone(sampleRate, func, 2.0, out data);
+            addTones(sampleCount, tone_1, tone_2, out data);
             writeData(sampleRate, "test.wav", data);
         }
     }
